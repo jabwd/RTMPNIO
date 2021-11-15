fd21930ea393763c14a8fbf69759d22c058d40ff
Antwan van Houdt <jabwd@exurion.com>
2021-11-15 04:17:32 +0100

    WIP: Decoder stuff

--- Sources/RTMPNIO/AMF/Decoder/AMFDecoder.swift
+++ Sources/RTMPNIO/AMF/Decoder/AMFDecoder.swift
@@ -6,3 +6,74 @@
 //
 
 import Foundation
+import NIO
+
+final public class AMFDecoder {
+    public init() {}
+
+    /**
+     A dictionary you use to customize the decoding process
+     by providing contextual information.
+     */
+    public var userInfo: [CodingUserInfoKey : Any] = [:]
+
+    // public func decode<T>(_ type: T.Type, from data: Data) throws -> T where T : Decodable {
+    //     switch type {
+    //         case is Data.Type:
+    //         let box = try Box<Data>(from: decoder)
+    //     }
+    // }
+}
+
+final class _AMFDecoder {
+    var codingPath: [CodingKey] = []
+    var userInfo: [CodingUserInfoKey : Any] = [:]
+
+    var container: AMFDecodingContainer?
+    fileprivate var buffer: ByteBuffer
+
+    init(buffer: ByteBuffer) {
+        self.buffer = buffer
+    }
+}
+
+protocol AMFDecodingContainer: AnyObject {
+    var codingPath: [CodingKey] { get set }
+    var userInfo: [CodingUserInfoKey : Any] { get }
+
+    var buffer: ByteBuffer { get set }
+}
+
+extension AMFDecodingContainer {
+    func readByte() throws -> UInt8 {
+        guard let byte = buffer.readBytes(length: 1)?.first else {
+            let context = DecodingError.Context(codingPath: self.codingPath, debugDescription: "Unexpected end of data")
+            throw DecodingError.dataCorrupted(context)
+        }
+        return byte
+    }
+
+    func readInteger<T: FixedWidthInteger>(as: T.Type) throws -> T {
+        guard let value = buffer.readInteger(endianness: .big, as: T.self) else {
+            let context = DecodingError.Context(codingPath: self.codingPath, debugDescription: "Unexpected end of data")
+            throw DecodingError.dataCorrupted(context)
+        }
+        return value
+    }
+
+    func readString(length: Int) throws -> String {
+        guard let value = buffer.readString(length: length) else {
+            let context = DecodingError.Context(codingPath: self.codingPath, debugDescription: "unexpected end of data while reading string")
+            throw DecodingError.dataCorrupted(context)
+        }
+        return value
+    }
+
+    func readMarker() throws -> AMF0TypeMarker {
+        guard let marker = AMF0TypeMarker(rawValue: try readByte()) else {
+            let context = DecodingError.Context(codingPath: self.codingPath, debugDescription: "Unknown type marker")
+            throw DecodingError.dataCorrupted(context)
+        }
+        return marker
+    }
+}
--- Sources/RTMPNIO/AMF/SingleValueDecodingContainer.swift
+++ Sources/RTMPNIO/AMF/SingleValueDecodingContainer.swift
@@ -37,6 +37,66 @@ extension _AMFDecoder.SingleValueContainer : SingleValueDecodingContainer {
     }
 
     func decode(_ type: String.Type) throws -> String {
+        let marker = try readMarker()
+
+        switch marker {
+        case .string:
+            let length: UInt16 = try readInteger(as: UInt16.self)
+            return try readString(length: Int(length))
+
+        case .longString:
+            let length: UInt32 = try readInteger(as: UInt32.self)
+            return try readString(length: Int(length))
+
+        default:
+            let context = DecodingError.Context(codingPath: codingPath, debugDescription: "Expected string got \(marker)")
+            throw DecodingError.typeMismatch(String.self, context)
+        }
         return ""
+    }
+
+    func decode(_ type: Double.Type) throws -> Double {
+        let marker = try readMarker()
+
+        switch marker {
+        case .number:
+            let value = try readInteger(as: UInt64.self)
+            return Double(bitPattern: value)
+        default:
+            let context = DecodingError.Context(codingPath: codingPath, debugDescription: "Expected number got \(marker)")
+            throw DecodingError.typeMismatch(String.self, context)
+        }
+    }
+
+    func decode(_ type: Float.Type) throws -> Float {
+        return Float(try decode(Double.self))
+    }
+
+    func decode<T>(_ type: T.Type) throws -> T where T : BinaryInteger & Decodable {
+        let double = try decode(Double.self)
+        guard let value = T(exactly: double) else {
+            let context = DecodingError.Context(codingPath: codingPath, debugDescription: "Unable to decode number to \(T.self)")
+            throw DecodingError.typeMismatch(T.self, context)
+        }
+        return value
+    }
+
+    func decode(_ type: Date.Type) throws -> Date {
+
+    }
+
+    func decode(_ type: Data.Type) throws -> Data {
+
+    }
+
+    func decode<T>(_ type: T.Type) throws -> T where T : Decodable {
+        let decoder = _AMFDecoder(buffer: self.buffer)
+        let value = try T(from: decoder)
+        /*
+         if let nextIndex = decoder.container?.index {
+            index = nextIndex
+        }
+        */
+        return value
     }
 }

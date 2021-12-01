5aa8fac330b34f2a0f8d799b1ae955a7fb7e00b7
Antwan van Houdt <jabwd@exurion.com>
2021-12-01 17:44:13 +0100

    Start amfdecoder

--- Sources/RTMPNIO/AMF/Decoder/AMF3/AMF3Decoder.swift
+++ Sources/RTMPNIO/AMF/Decoder/AMF3/AMF3Decoder.swift
@@ -1,3 +1,20 @@
-final class AMF3Decoder {
+import NIO
 
+final class AMF3Decoder {
+    func decode<T>(_: T.Type, from buffer: ByteBuffer) throws -> T where T: Decodable {
+        fatalError("Not implemented")
+    }
+}
+
+final class _AMF3Decoder {
+    var codingPath: [CodingKey] = []
+    var userInfo: [CodingUserInfoKey: Any] = [:]
+    var container: AMF3DecodingContainer?
+    fileprivate var buffer: ByteBuffer
+    let referenceTable: AMF3ReferenceTable
+
+    init(buffer: ByteBuffer, referenceTable: AMF3ReferenceTable) {
+        self.buffer = buffer
+        self.referenceTable = referenceTable
+    }
 }
